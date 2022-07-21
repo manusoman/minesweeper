6e3585648dfd400c5d9592176ff9ce666c461eed
Manu Soman <manu.naturep@gmail.com>
2022-07-21 15:37:45 +0530

    game creation added, bugs found

--- app.js
+++ app.js
@@ -1,24 +1,54 @@
 (() => { 'use strict';
 
-putCopyright();
+const configs = [
+    { type : 'small', size : 14 },
+    { type : 'medium', size : 22 },
+    { type : 'large', size : 30 },
+];
 
 const mineRate = 0.16;
-const rows = 14;
-const cols = 14;
+const colorClasses = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
 
 let mineFreeBoxes;
 let openedBoxes = 0;
 
-const hasMine = () => Math.random() <= mineRate ? 'm' : 0;
-const colorClasses = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
-const gameEle = document.getElementById('game');
-const gameArray = createGame(rows, cols);
-const gameGrid = createGrid(gameEle);
-
-attachBoxEvents();
+putCopyright();
+addCreateGameEvents(configs);
 
 
 // Functions *************************************************************************
+
+const hasMine = () => Math.random() <= mineRate ? 'm' : 0;
+
+function addCreateGameEvents(configs) {
+    const gameEle = document.getElementById('game');
+    const configureEle = document.getElementById('configure');
+    let gameFrame;
+
+    const gameFinish = isSuccess => {
+        isSuccess ? alert('Success!') : alert('You lost :(');
+        gameEle.removeChild(gameFrame);
+        gameEle.classList.add('off');
+        configureEle.classList.remove('off');
+    };
+
+    configs.forEach(conf => {
+        document.getElementById(conf.type).addEventListener('click', e => {            
+            gameFrame = document.createElement('div');
+            openedBoxes = 0;
+
+            const gameArray = createGame(conf.size, conf.size);
+            const gameGrid = createGrid(gameFrame, gameArray);
+
+            attachBoxEvents(gameGrid, gameArray, gameFinish);
+
+            gameEle.appendChild(gameFrame);
+            configureEle.classList.add('off');
+            gameEle.classList.remove('off');
+            e.stopPropagation();
+        }, true);
+    });
+}
 
 function createGame(rows, cols) {
     const gameArray = [];
@@ -60,8 +90,10 @@ function createGame(rows, cols) {
     return gameArray;
 }
 
-function createGrid(gameEle) {
+function createGrid(gameEle, gameArray) {
     const fragment = new DocumentFragment();
+    const rows = gameArray.length;
+    const cols = gameArray[0].length;
     const gameGrid = [];
 
     for (let i = 0; i < rows; ++i) {
@@ -91,27 +123,42 @@ function createGrid(gameEle) {
     return gameGrid;
 }
 
-function attachBoxEvents() {
+function attachBoxEvents(gameGrid, gameArray, gameFinishCB) {
+    const rows = gameArray.length;
+    const cols = gameArray[0].length;
+
+    const openBox = (i, j, showCount = true) => {
+        if (showCount) gameGrid[i][j].textContent = gameArray[i][j];
+        gameGrid[i][j].classList.add('open');
+        gameArray[i][j] = 'o';
+
+        ++openedBoxes === mineFreeBoxes && gameFinishCB(true);
+    };
+
     for (let i = 0; i < rows; ++i) {
         for (let j = 0; j < cols; ++j) {
             gameGrid[i][j].addEventListener('click', e => {
                 e.stopPropagation();
-                handleClick(i, j);
+                handleClick(i, j, gameArray, gameFinishCB, openBox);
             }, true);
         }
     }
 }
 
-function handleClick(i, j) {
+function handleClick(i, j, gameArray, gameFinishCB, openBox) {
     if (gameArray[i][j] === 'o') return;
-    if (gameArray[i][j] === 'm') throw 'Opened a mine: Game over!';
+
+    if (gameArray[i][j] === 'm') {
+        gameFinishCB(false);
+        return;
+    }
 
     if (gameArray[i][j] === 0) {
         openBox(i, j, false);
 
         for (let temp_i = i - 1; temp_i <= i + 1; ++temp_i) {
             for (let temp_j = j - 1; temp_j <= j + 1; ++temp_j) {
-                if(gameArray[temp_i] === undefined || gameArray[temp_i][temp_j] === undefined) continue;
+                if (gameArray[temp_i] === undefined || gameArray[temp_i][temp_j] === undefined) continue;
 
                 if (gameArray[temp_i][temp_j] === 0) handleClick(temp_i, temp_j);
                 else if (gameArray[temp_i][temp_j] !== 'o') openBox(temp_i, temp_j);
@@ -120,17 +167,8 @@ function handleClick(i, j) {
     } else openBox(i, j);
 }
 
-function openBox(i, j, showCount = true) {
-    if (showCount) gameGrid[i][j].textContent = gameArray[i][j];
-    gameGrid[i][j].classList.add('open');
-    gameArray[i][j] = 'o';
-
-    ++openedBoxes === mineFreeBoxes && alert('Success!');
-}
-
 function putCopyright() {
-    let year = null;
-    let spanEle;
+    let spanEle, year;
 
     fetch('https://manusoman.github.io/MindLogs/settings.json')
 	.then(res => res.json())
