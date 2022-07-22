9d269def12d6facdf45098152a864a08f23be30b
Manu Soman <manu.naturep@gmail.com>
2022-07-22 12:15:09 +0530

    Bug fixed, basic version completed

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 (() => { 'use strict';
 
 const configs = [
-    { type : 'small', size : 14 },
-    { type : 'medium', size : 22 },
-    { type : 'large', size : 30 },
+    { type : 'small', rows : 14, cols : 14 },
+    { type : 'medium', rows : 22, cols : 22 },
+    { type : 'large', rows : 30, cols : 30 }
 ];
 
 const mineRate = 0.16;
@@ -33,15 +33,14 @@ function addCreateGameEvents(configs) {
     };
 
     configs.forEach(conf => {
-        document.getElementById(conf.type).addEventListener('click', e => {            
+        document.getElementById(conf.type).addEventListener('click', e => {
             gameFrame = document.createElement('div');
             gameFrame.classList.add(conf.type);
             openedBoxes = 0;
 
-            const gameArray = createGame(conf.size, conf.size);
-            const gameGrid = createGrid(gameFrame, gameArray);
+            const gameGrid = createGrid(gameFrame, conf.rows, conf.cols);
 
-            attachBoxEvents(gameGrid, gameArray, gameFinish);
+            attachBoxEvents(gameGrid, gameFinish);
             gameEle.appendChild(gameFrame);
             configureEle.classList.add('off');
             gameEle.classList.remove('off');
@@ -50,19 +49,105 @@ function addCreateGameEvents(configs) {
     });
 }
 
-function createGame(rows, cols) {
+function createGrid(parentEle, rows, cols) {
+    const gameGrid = [];
+
+    for (let i = 0; i < rows; ++i) {
+        const row = document.createElement('div');
+        const rowArray = [];
+        
+        for (let j = 0; j < cols; ++j) {
+            const box = document.createElement('div');
+
+            box.classList.add('box');
+            row.appendChild(box);
+            rowArray.push(box);
+        }
+
+        row.classList.add('row');
+        parentEle.appendChild(row);
+        gameGrid.push(rowArray);
+    }
+    
+    return gameGrid;
+}
+
+function attachBoxEvents(gameGrid, gameFinishCB) {
+    const rows = gameGrid.length;
+    const cols = gameGrid[0].length;
+    let gameArray;
+
+    for (let i = 0; i < rows; ++i) {
+        for (let j = 0; j < cols; ++j) {
+            gameGrid[i][j].addEventListener('click', e => {
+                e.stopPropagation();
+
+                if (!gameArray) {
+                    gameArray = createGame(rows, cols, i, j);
+                    styleMineCounts(gameGrid, gameArray);
+                }
+                
+                handleClick(i, j);
+            }, true);
+        }
+    }
+
+    function handleClick(i, j) {
+        const val = gameArray[i][j];
+    
+        if (val === 'o') return;
+    
+        if (val === 'm') {
+            gameFinishCB(false);
+            return;
+        }
+    
+        openBox(i, j, val);
+    
+        if (val > 0) return;
+    
+        for (let temp_i = i - 1; temp_i <= i + 1; ++temp_i) {
+            for (let temp_j = j - 1; temp_j <= j + 1; ++temp_j) {
+                const temp_val = gameArray[temp_i] === undefined ||
+                                    gameArray[temp_i][temp_j] === undefined ?
+                                    undefined : gameArray[temp_i][temp_j];
+    
+                if (temp_val === undefined) continue;
+    
+                if (temp_val === 0) handleClick(temp_i, temp_j);
+                else if (temp_val !== 'o') openBox(temp_i, temp_j, temp_val);
+            }
+        }
+    }
+    
+    function openBox(i, j, val) {
+        const box = gameGrid[i][j];
+
+        if (val > 0) box.textContent = val;
+
+        box.classList.add('open');
+        gameArray[i][j] = 'o';
+
+        ++openedBoxes === mineFreeBoxes && gameFinishCB(true);
+    }
+}
+
+function createGame(rows, cols, clickedRow, clickedCol) {
     const gameArray = [];
     let totalMines = 0;
-    let r = rows;
 
-    while (r--) {
+    for (let i = 0; i < rows; ++i) {
         const a = [];
-        let i = cols;
+        const zeroFlag = (i >= clickedRow - 1) && (i <= clickedRow + 1);
 
-        while (i--) {
-            const temp = hasMine();
-            temp && ++totalMines;
-            a.push(temp);
+        for (let j = 0; j < cols; ++j) {
+            if (zeroFlag && (j >= clickedCol - 1) && (j <= clickedCol + 1)) {
+                a.push(0);
+            } else {
+                const temp = hasMine();
+                temp && ++totalMines;
+                a.push(temp);
+            }
         }
 
         gameArray.push(a);
@@ -90,81 +175,20 @@ function createGame(rows, cols) {
     return gameArray;
 }
 
-function createGrid(gameEle, gameArray) {
-    const fragment = new DocumentFragment();
-    const rows = gameArray.length;
-    const cols = gameArray[0].length;
-    const gameGrid = [];
+function styleMineCounts(gameGrid, gameArray) {
+    const rows = gameGrid.length;
+    const cols = gameGrid[0].length;
 
     for (let i = 0; i < rows; ++i) {
-        const row = document.createElement('div');
-        const rowArray = [];
-
-        row.classList.add('row');
-        
         for (let j = 0; j < cols; ++j) {
-            const box = document.createElement('div');
             const val = gameArray[i][j];
 
             if (val && val !== 'm') {
+                const box = gameGrid[i][j];
                 box.classList.add(colorClasses[val - 1]);
             }
-
-            box.classList.add('box');
-            row.appendChild(box);
-            rowArray.push(box);
-        }
-
-        fragment.appendChild(row);
-        gameGrid.push(rowArray);
-    }
-    
-    gameEle.appendChild(fragment);
-    return gameGrid;
-}
-
-function attachBoxEvents(gameGrid, gameArray, gameFinishCB) {
-    const rows = gameArray.length;
-    const cols = gameArray[0].length;
-
-    const openBox = (i, j, showCount = true) => {
-        if (showCount) gameGrid[i][j].textContent = gameArray[i][j];
-        gameGrid[i][j].classList.add('open');
-        gameArray[i][j] = 'o';
-
-        ++openedBoxes === mineFreeBoxes && gameFinishCB(true);
-    };
-
-    for (let i = 0; i < rows; ++i) {
-        for (let j = 0; j < cols; ++j) {
-            gameGrid[i][j].addEventListener('click', e => {
-                e.stopPropagation();
-                handleClick(i, j, gameArray, gameFinishCB, openBox);
-            }, true);
         }
     }
-}
-
-function handleClick(i, j, gameArray, gameFinishCB, openBox) {
-    if (gameArray[i][j] === 'o') return;
-
-    if (gameArray[i][j] === 'm') {
-        gameFinishCB(false);
-        return;
-    }
-
-    if (gameArray[i][j] === 0) {
-        openBox(i, j, false);
-
-        for (let temp_i = i - 1; temp_i <= i + 1; ++temp_i) {
-            for (let temp_j = j - 1; temp_j <= j + 1; ++temp_j) {
-                if (gameArray[temp_i] === undefined || gameArray[temp_i][temp_j] === undefined) continue;
-
-                if (gameArray[temp_i][temp_j] === 0) handleClick(temp_i, temp_j, gameArray, gameFinishCB, openBox);
-                else if (gameArray[temp_i][temp_j] !== 'o') openBox(temp_i, temp_j);
-            }
-        }
-    } else openBox(i, j);
 }
 
 function putCopyright() {
